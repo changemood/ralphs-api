09fe1617169aed3d1423da6eda4499279a99f273
naohiro-t <takahashi.naohiro1222@gmail.com>
2018-01-04 22:51:48 +0900

    update user table

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20180102141230) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "name"
-    t.string "nickname"
     t.string "image"
     t.string "email"
     t.json "tokens"
