be5255f3d716e001928a23020155c6eb487a0bcc
naohiro-t <takahashi.naohiro1222@gmail.com>
2018-02-24 16:34:39 +0900

    add seeds for sr_event

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# user = User.new({email: "localhost+0@ralph.com", password: "password"}).save!
-# board = user.boards.create({name: "Board"})
-# board.card.create({title: "Card", body: "Body"})
-# User.new({email: "localhost+#{no}@ralph.com", password: "password"}).save!
-# Store.create({
-#   user_id: user.id
-#   store_id: store.id
-# }, without_protection: true)
 
+# This is only for test and development
 5.times do |no|
   User.new({email: "localhost+#{no}@ralph.com", password: "password"}).save!
   User.first.boards.new({name: "Board #{no}"}).save!
-  Board.first.cards.new({title: "Card #{no}", body: "Body #{no}", user_id: User.first.id}).save!  
+  Board.first.cards.new({title: "Card #{no}", body: "Body #{no}", user_id: User.first.id}).save!
+  Card.first.sr_events.new(interval: 4, review_count: 1, next_review_at: Time.now + 4.hours, user_id: User.first.id).save!
 end
